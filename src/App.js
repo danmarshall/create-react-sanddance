69532ca1e6d3d3be81d070d0f624618af3aaeafb
Dan Marshall <danmar@microsoft.com>
2023-10-31 10:59:53 -0700

    remove unused files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import * as SandDanceReact from '@msrvida/sanddance-react';
+import * as vega from 'vega';
+import * as fluentui from '@fluentui/react';
+import { Explorer, use } from '@msrvida/sanddance-explorer';
+
+import "@msrvida/sanddance-explorer/dist/css/sanddance-explorer.css";
+import "./style.css";
+
+
 
 function App() {
+  SandDanceReact.use(React, ReactDOM, vega);
+  fluentui.initializeIcons();
+  use(fluentui, React, ReactDOM, vega);
+  const data = [
+    { a: 1, b: "c1" },
+    { a: 1, b: "c2" },
+    { a: 2, b: "c3" },
+    { a: 3, b: "c4" }
+  ];
+
+  const explorerProps = {
+    logoClickUrl: 'https://microsoft.github.io/SandDance/',
+    mounted: explorer => {
+      explorer.load(data);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Main App Component</h1>
+      <div id="innerContainer">
+        <Explorer {...explorerProps} />
+      </div>
     </div>
   );
 }
